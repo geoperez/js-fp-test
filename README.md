75bfbbd67e3d60d4e0ca01b5e361d77dd288418b
Geo Perez <1775792+geoperez@users.noreply.github.com>
2019-05-23 15:25:31 -0500

    Final

--- README.md
+++ README.md
@@ -9,7 +9,9 @@ Create a `Transformer` object with a `transform` function with the following spe
 * `transform` will receive an object and output another object.
 * All the functions inside `Transformer` MUST be pure functions.
 * Functions MUST have less of two arguments.
-* All data structures MUST be immutable.
+* All data structures MUST be immutable. (You SHOULD NOT use [mutator methods](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/prototype#Mutator_methods)).
+* Cannot use the `Set` object.
+* Reuse code.
 * The statements `if-else`, `for`, `for-in`, `while` and `do-while` are forbidden.
 
 The `Transformer` should be exported as the default value in the index file of the src folder.
--- src/index.js
+++ src/index.js
@@ -1,7 +1,58 @@
-export default {
-    transform: (data) => {
-        console.log(data);
+function select(array, property) {
+    return array.map(a => a[property]);
+}
 
-        return data;
-    }
+function max(array) {
+    return array.reduce((maxValue, currentValue) => maxValue > currentValue ? maxValue : currentValue);
+}
+
+function selectFlat(array, property) {
+    return array.reduce((x, y) => [...x, ...y[property]], []);
+}
+
+function selectFloat(array, property) {
+    return array.map(a => parseFloat(a[property]));
+}
+
+function distinct(value, index, self) {
+    return self.indexOf(value) == index;
+}
+
+function countArray(array, value) {
+    return array.filter(y => y == value).length;
+}
+
+function reduceCountObject(array) {
+    return (obj, property) => ({...obj, [property]: countArray(array, property)});
+}
+
+function countProperties(array) {
+    return array
+        .filter(distinct)
+        .reduce(reduceCountObject(array), {});
+}
+
+function selectLatitude(data) {
+    return selectFloat(data, 'latitude');
+}
+
+function maxLatitude(data) {
+    return max(selectLatitude(data));
+}
+
+function countLatitude(data) {
+    return countProperties(selectLatitude(data));
+}
+
+function countTags(data) {
+    return countProperties(selectFlat(data, 'tags'));
+}
+
+export default {
+    transform: (data) =>
+        ({
+            maxLatitude: maxLatitude(data),
+            countLatitude: countLatitude(data),
+            countTags: countTags(data)
+        })
 }
